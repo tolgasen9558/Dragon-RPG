58e76deff04e1767b3e337d738237247dfa67be7
Tolga Şen <tolgasen9558@gmail.com>
2017-04-22 21:55:36 +0300

    Raycasting for mouse click and move implemented, prefabbed objects on the scene, created another scene called village (for test purpose)

---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof (ThirdPersonCharacter))]
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField] float walkMoveStopRadius = 0.2f;
+
+    ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
+    CameraRaycaster cameraRaycaster;
+    Vector3 currentClickTarget;
+
+    private void Start()
+    {
+        cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+        m_Character = GetComponent<ThirdPersonCharacter>();
+        currentClickTarget = transform.position;
+    }
+
+    // Fixed update is called in sync with physics
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            print("Cursor raycast hit layer" + cameraRaycaster.layerHit.ToString());
+			switch (cameraRaycaster.layerHit){
+				case Layer.Walkable:
+					currentClickTarget = cameraRaycaster.hit.point; 
+					break;
+				case Layer.Enemy:
+					print("Not moving to enmey");
+					break;
+				default:
+					print("Unexpected layer to move");
+					return;
+			}
+        }
+
+		var playerToClickPoint = currentClickTarget - transform.position;
+		if(playerToClickPoint.magnitude >= walkMoveStopRadius){
+			m_Character.Move(playerToClickPoint, false, false);
+		}
+		else{
+			m_Character.Move(Vector3.zero, false, false);
+		}
+    }
+}
+
